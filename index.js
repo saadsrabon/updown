12066bf4924700d3969fc0bc9cd067e769bd3d0f
Saad Srabon <saadsrabon2@gmail.com>
2025-06-06 11:30:53 +0600

    files

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-
-
-const app ={}
-app.config = {
-    port: 3000,
-}
-
 // Dependecies
-const http = require('http');
- app.createServer = () => {
-const server = http.createServer(app,app.handleRequest);
-server.listen(app.config.port, () => {
+const http = require("http");
+// app scafold
+const app = {};
+app.config = {
+  port: 3000,
+};
+
+app.createServer = () => {
+  const server = http.createServer(app, app.handleRequest);
+  server.listen(app.config.port, () => {
     console.log(`Server is running on http://localhost:${app.config.port}`);
-});
-}
+  });
+};
 app.handleRequest = (req, res) => {
-    // Handle incoming requests
-    // console.log(`Received request for ${req.url}`);
-    // res.end('Hello, World!');
-    // You can add more logic here to handle different routes or methods
-    if (req.method === 'GET' && req.url === '/about') {
-        
-        res.end('Hello, from the about page!');
-    }
-
-    
-
+  //parse url
+  //path
+  //query
+  //method 
+  //headers 
+  //body
+  // decoder for buffer data
 };
 
 app.createServer();
