d586efe34aa55375985de53e3b11aef95b929b32
Saad Srabon <saadsrabon2@gmail.com>
2025-06-09 13:37:45 +0600

    Main files done

--- helpers/envVarables.js
+++ helpers/envVarables.js
@@ -9,6 +9,11 @@ environment.production = {
     port: 5000,
     envName: "production",
 };
-const currentEnvironment = typeof process.env.NODE_ENV == "string" ? process.env.NODE_ENV : "staging";
+// const currentEnvironment = typeof process.env.NODE_ENV == "string" ? process.env.NODE_ENV : "staging";
+// const environmentToExport = typeof environment[currentEnvironment] == "object" ? environment[currentEnvironment] : environment.staging;
+// module.exports = environmentToExport;
+
+
+const currentEnvironment = typeof process.env.NODE_ENV =="string" ? process.env.NODE_ENV : "staging";
 const environmentToExport = typeof environment[currentEnvironment] == "object" ? environment[currentEnvironment] : environment.staging;
 module.exports = environmentToExport;
